5aeaecf40371887ecf15584af6cdf203a608bc6e
q2apro.com <info@q2apro.com>
2018-06-21 16:15:06 +0300

    init

---
+++ README.rst
@@ -0,0 +1,49 @@
+==================================================
+Question2Answer Plugin: Question Caching Plugin
+==================================================
+-----------
+Description
+-----------
+This is a plugin_ for Question2Answer_ that caches each question in the file system.
+
+------------
+Installation
+------------
+#. Install Question2Answer_ if you haven't already.
+#. Get the source code for this plugin directly from github_.
+#. Extract the files.
+#. Upload the files to a subfolder called ``q2apro-caching`` inside the ``qa-plugin`` folder of your Q2A installation.
+#. *Create a folder `qa-cache` in the root of your q2a installation.
+#. Do NOT RENAME the folder ``q2apro-caching``.
+#. Navigate to your site, go to **Admin -> Plugins**. Check if the plugin "q2apro caching" is listed and *enable* it.
+
+----------
+Notes
+----------
+
+This is a fork from [bndr](https://github.com/bndr/q2a-caching) + [sama55](https://github.com/sama55) + [stevenev](https://github.com/stevenev/q2a-caching).
+
+----------
+Disclaimer
+----------
+This is **beta** code. It is probably okay for production environments, but may not work exactly as expected. You bear the risk. Refunds will not be given!
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+---------
+Copyright
+---------
+All code herein is OpenSource_. Feel free to build upon it and share with the world.
+Wait a moment, the original author (Vadim Kr. bndr) put it under [CC BY-SA](https://creativecommons.org/licenses/by-sa/3.0/legalcode)
+
+---------
+About q2a
+---------
+Question2Answer_ is a free and open source PHP software for Q&A sites.
+
+  
+.. _github: https://github.com/q2apro/q2apro-change-post-owner
+.. _OpenSource: http://www.gnu.org/licenses/gpl.html
+.. _Question2Answer: http://www.question2answer.org/
---
+++ qa-caching-event.php
@@ -0,0 +1,89 @@
+<?php
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+    header('Location: ../../');
+    exit;
+}
+require_once QA_PLUGIN_DIR.'q2apro-caching/qa-caching-main.php';
+
+class qa_caching_event
+{	
+    public function process_event($event, $userid, $handle, $cookieid, $params)
+	{
+		// update each question on changes made
+		if(!empty($params['postid']))
+		{
+			$postid = $params['postid'];
+			
+			// *** $parentid = $params['parentid'] // might be used for better performance
+			
+			// get post data
+			$postdata = qa_db_read_one_assoc(
+							qa_db_query_sub('
+								SELECT type, parentid FROM `^posts` 
+								WHERE `postid` = #
+								', $postid), true);
+			$questionid = null;
+			
+			if(!empty($postdata['type']))
+			{
+				if($postdata['type']=='Q')
+				{
+					$questionid = $postid;
+				}
+				else if($postdata['type']=='A' && !empty($postdata['parentid']))
+				{
+					$questionid = $postdata['parentid'];
+				}
+				else if($postdata['type']=='C' && !empty($postdata['parentid']))
+				{
+					// C parent is A or Q - get post data of parent 
+					$parentdata = qa_db_read_one_assoc(
+										qa_db_query_sub('
+											SELECT type, parentid FROM `^posts` 
+											WHERE `postid` = #
+											', $postdata['parentid']), true);
+					if($parentdata['type']=='Q')
+					{
+						$questionid = $postdata['parentid'];
+					}
+					else if($parentdata['type']=='A')
+					{
+						// A parent is always Q
+						$questionid = $parentdata['parentid'];
+					}
+				}
+			}
+			
+			if(!empty($questionid))
+			{
+				$main = new qa_caching_main;
+				$main->clear_cache($questionid);
+			}
+		}
+		/*
+		// do not clear entire cache anymore
+		else
+		{
+			// clears entire cache
+			$main = new qa_caching_main;
+			$main->clear_cache();
+		}
+		*/
+        
+    } // END process_event
+}
+
+class qa_caching_session_reset_event 
+{
+	public function process_event($event, $userid, $handle, $cookieid, $params) 
+	{
+		if($event == 'u_login')
+		{
+			// turn off the flag if login happened
+			if(isset($_SESSION['cache_use_off']))
+			{
+				unset($_SESSION['cache_use_off']);
+			}
+		}
+	}
+}
